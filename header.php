478f0462330e3d55bd937783916e58117c5505cf
Sarvaiyasmit <sssarvaiya76@gmail.com>
2026-03-13 21:29:07 +0530

    Add header component

---
+++ header.php
@@ -0,0 +1,20 @@
+   <header>
+        <div class="header-content">
+            <div class="logo-area">
+                <h1 class="gym-text">FITNESS CLUB</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="indexpage.php" id="nav-home-link">Home</a></li>
+                    <li><a href="Page2.php">Blog</a></li>
+                    <li><a href="Page3.php">Services</a></li>
+                    <li><a href="Page4.php">Register</a></li>
+                    <li><a href="Page7.php">Login</a></li>
+                    
+                    <li><a href="Page6.php">Contact</a></li>
+                    <li><a href="Page5.php">About us</a></li>
+                    <li><a href="admin_login.php" style="color:#333; font-weight:700;">⚙ Admin</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
