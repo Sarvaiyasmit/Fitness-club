a5e21bcbe990ec68e02dcc2c63dd93a5edd63a6f
Sarvaiyasmit <sssarvaiya76@gmail.com>
2026-03-17 23:49:44 +0530

    Add a new page in to  website : Blog

---
+++ Page2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blog</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+    <style>
+    </style>
+</head>
+<body>
+  
+    <?php include 'header.php'; ?>
+        <div class="blog-content">
+            <h1>FROM OUR BLOG</h1>
+            <p class="blog-text">List of all news and events that take place related to us</p>
+        </div>
+
+    <div class="container">
+  
+    <div class="card">
+      <img src="images/khabib.webp" alt="Gym Training">
+      <div class="card-text">
+        <div class="category">KHABIB</div>
+        <div class="title">NEW UFC TRAINING IS ALSO IN OUR GYM</div>
+     
+      </div>
+    </div>
+
+    
+    <div class="card">
+      <img src="images/yoga room.webp" alt="Workout">
+      <div class="card-text">
+        <div class="category">YOGA</div>
+        <div class="title">BEUTIFULL YOGA ROOM IS IN OUR GYM</div>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="images/ground.avif" alt="Fitness">
+      <div class="card-text">
+        <div class="category">GROUND</div>
+        <div class="title">JUST NEW GROUND ADD FOR RUNNING </div>
+      </div>
+    </div>
+    </div>
+
+    <?php include 'footer.php'; ?>
+    
+   
+    
+</body>
+</html>
